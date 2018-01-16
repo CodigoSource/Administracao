709994d63a2202ea3d8a14f82cf07b97498296ba
Djonata Wehmuth <djonata1098@gmail.com>
2018-01-15 22:31:53 -0200

    v 0.01

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,45 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'administracao', 'before' => 'auth'], function() {
+
+    Route::group(['prefix' => 'pessoas'], function() {
+        Route::get('index', 'PessoaController@index');
+        Route::get('consultar/{id}', 'PessoaController@consultar');
+        Route::post('gravar', 'PessoaController@gravar');
+    });
+
+    Route::group(['prefix' => 'usuarios'], function() {
+        Route::get('index', 'UserController@index');
+        Route::get('consultar/{id}', 'UserController@consultar');
+        Route::post('gravar', 'UserController@gravar');
+    });
+
+    Route::group(['prefix' => 'produtos'], function() {
+        Route::get('index', 'ProdutoController@index');
+        Route::get('consultar/{id}', 'ProdutoController@consultar');
+        Route::post('gravar', 'ProdutoController@gravar');
+    });
+
+    Route::group(['prefix' => 'operacoes'], function() {
+        Route::get('index', 'OperacaoController@index');
+        Route::get('consultar/{id}', 'OperacaoController@consultar');
+        Route::post('gravar', 'OperacaoController@gravar');
+    });
+
+    Route::group(['prefix' => 'sites'], function() {
+        Route::get('index', 'SiteController@index');
+        Route::get('consultar/{id}', 'SiteController@consultar');
+        Route::post('gravar', 'SiteController@gravar');
+    });
+});
+
+
+/* Modelo de rotas
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('posts', 'PostController@index');
+    Route::get('accounts', 'AccountController@index');
+    Route::post('dashboard', 'DashboardController@index')
+});
+ */
